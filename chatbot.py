26a14aaa0464daa4eaadb9bfd3516ac0e39543a0
Akshay <akshayshenvi@gmail.com>
2019-03-15 13:08:46 -0500

    till hyperparameters initialized and start of training

--- chatbot.py
+++ chatbot.py
@@ -268,3 +268,57 @@ def seq2seq_model(inputs, targets, keep_prob, batch_size, sequence_length, answe
                                                          batch_size)
     return training_predictions, test_predictions
     
+
+# Training the Model
+
+# Setting the hyperparameters
+
+epochs = 100
+batch_size = 64
+rnn_size =512
+num_layers = 3
+encoding_embedding_size = 512
+decoding_embedding_size = 512
+learnung_rate =0.01
+learning_rate_decay = 0.9
+min_learning_rate =0.0001
+keep_probability = 0.5
+
+#Defining a session
+tf.reset_default_graph()
+session =tf.InteractiveSession()
+
+#Loading the model inputs
+inputs, targets, lr, keep_prob = model_inputs()
+
+#Setting the sequence length
+sequence_length = tf.placeholder_with_default(25,None,name ='sequence_length')
+
+#getting the sahpe of the input tensor
+input_shape = tf.shape(inputs)
+
+#getting the training predictions and test predictions
+training_predictions, test_predictions = seq2seq_model(tf.reverse(inputs,[-1]),
+                                                       targets,
+                                                       keep_prob,
+                                                       batch_size,
+                                                       sequence_length,
+                                                       len(answerswordstoint),
+                                                       len(questionswordstoint),
+                                                       encoding_embedding_size,
+                                                       decoding_embedding_size,
+                                                       rnn_size,
+                                                       num_layers,
+                                                       questionswordstoint)
+
+
+
+#Loss error, Optimizer and Gradient Clipping
+with tf.name_scope("optimization"):
+    loss_error= tf.contrib.seq2seq.sequence_loss(training_predictions,
+                                                 targets,
+                                                 tf.ones([input_shape[0],sequence_length]))
+    optimizer =  tf.train.AdamOptimizer(learnung_rate)
+    gradients = optimizer.compute_gradients(loss_error)
+    clipped_gradients = [(tf.clip_by_value(grad_tensor,-5.0,5.0), grad_variable) for grad_tensor, grad_variable in gradients if grad_tensor is not None] 
+    optimizer_gradient_clipping = optimizer.apply_gradients(clipped_gradients)
